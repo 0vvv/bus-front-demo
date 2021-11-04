75cf64d0f5a08129f7b8a24e300a53ce417d58ff
ysygMhdxw <75829561+ysygMhdxw@users.noreply.github.com>
2021-11-04 22:26:39 +0800

    update

--- src/views/DirectPathByRoute.vue
+++ src/views/DirectPathByRoute.vue
@@ -1,90 +1,90 @@
 <template>
-    <div style="padding: 10px">
-        <div style="margin: 10px 0">
-            站点1:
-            <el-input v-model="search1" placeholder="请输入站点1的名称……" style="width: 25%" clearable/>
-            <tr/>
-            站点2:
-            <el-input v-model="search2" placeholder="请输入站点2的名称……" style="width: 25%" clearable/>
-            <tr/>
-            线路:
-            <el-input v-model="search3" placeholder="请输入线路的名称……" style="width: 25%" clearable/>
-            <el-button type="primary" style="margin: 5px" @click="findPath">搜索途径站点</el-button>
-        </div>
-        <div class="block">
-            <el-timeline v-for="eachroute in route" style="padding-left: 20px">
-                <div>
-                    <el-tag :key="eachroute.name">
-                        {{ eachroute.name }}
-                    </el-tag>
-                </div>
-
-                <el-timeline-item
-                        v-for="(station, index) in eachroute.stations"
-                        :key="index"
-                        color="#507cff"
-                        size='large'
-                        :timestamp="index"
-                        center="true"
-                >
-                    {{ station.name }}
-                </el-timeline-item>
-            </el-timeline>
-        </div>
+  <div style="padding: 10px">
+    <div style="margin: 10px 0">
+      站点1:
+      <el-input v-model="search1" placeholder="请输入站点1的名称……" style="width: 25%" clearable/>
+      <tr/>
+      站点2:
+      <el-input v-model="search2" placeholder="请输入站点2的名称……" style="width: 25%" clearable/>
+      <tr/>
+      线路&nbsp&nbsp:
+      <el-input v-model="search3" placeholder="请输入线路的名称……" style="width: 25%" clearable/>
+      <el-button type="primary" style="margin: 5px" @click="findPath">搜索途径站点</el-button>
     </div>
+    <div class="block">
+      <el-timeline v-if="showTimeline" v-for="eachroute in route" style="padding-left: 20px">
+        <div>
+          <el-tag :key="eachroute.name">
+            线路名称：{{ eachroute.name }}
+          </el-tag>
+          <br>
+          <el-tag :key="eachroute.time">
+            运行时间：{{ eachroute.time }}
+          </el-tag>
+          <br>
+
+        </div>
+        <el-timeline-item
+            v-for="(station, index) in eachroute.stations"
+            :key="index"
+            color="#507cff"
+            size='large'
+            :timestamp="station.myId"
+            center="true"
+        >
+          {{ station.name }}
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </div>
 </template>
 
 <script>
-    import request from "@/utils/request";
+import request from "@/utils/request";
 
-    export default {
-        name: "DirectPathByRoute",
-        components: {},
-        data() {
-            return {
-                loading: true,
-                search1: '',
-                search2: '',
-                search3: '',
-                route: [],
-                // activities: [
-                //     {
-                //         // center: true,   横向时间轴
-                //     },
-                // ],
-                tags: [
-                    // { name: 'Tag 1', type: '' },
-                ],
-            }
-        }, created() {  //页面加载的时候调用
-            this.findPath();
-        },
-        methods: {
-            findPath() {
-                this.loading = true
-                request.get("getTwoStationOnThisPathDirectPathByName", {
-                    params: {
-                        name1: this.search1,
-                        name2: this.search2,
-                        routename: this.search3,
-                    }
-                }).then(res => {
-                    this.tags = []
-                    // this.activities = []
-                    this.route = []
-                    console.log(res)
-                    this.loading = false
-                    this.route = res.data
-                    // res.data.forEach(item => {
-                    //     this.tags.push({name: item.name})
-                    //     item.stations.forEach(station =>
-                    //         this.activities.push({content: station.name, size: 'large', color: '#507cff', center: true})
-                    //     )
-                    // })
-                })
-            }
-        },
+export default {
+  name: "DirectPathByRoute",
+  components: {},
+  data() {
+    return {
+      loading: true,
+      search1: '',
+      search2: '',
+      search3: '',
+      showTimeline:false,
+      route: [],
+      tags: [
+        // { name: 'Tag 1', type: '' },
+      ],
     }
+  }, created() {  //页面加载的时候调用
+    this.findPath();
+  },
+  methods: {
+    findPath: function () {
+      this.loading = true
+      request.get("/StationLine/lineofstartansendstation/" + this.search3 + ":" + this.search1 + "-" + this.search2, {
+        params: {
+          name1: this.search1,
+          name2: this.search2,
+          routename: this.search3,
+        }
+      }).then(res => {
+        this.tags = []
+        this.route = []
+        console.log(res)
+        console.log(res.data)
+        if (res.data.code == "00000") {
+          this.$message.success("查询成功！")
+          this.showTimeline=true
+          this.route = res.data.data
+        } else if (this.search1!="" && this.search2!=" " && res.data.code == "A0001")
+          this.$message.warning("不存在直达线路！请输入新的数据再次尝试！")
+
+      })
+    }
+  },
+}
 
 </script>
 
--- src/views/DirectRoute.vue
+++ src/views/DirectRoute.vue
@@ -1,58 +1,58 @@
 <template>
-    <!--    给两个站点，返回直达的公交车路线名-->
-    <div style="padding: 10px">
-        <!--        搜索框-->
-        <div style="margin: 10px 0">
-            站点1:
-            <el-input v-model="search1" placeholder="请输入站点1的名称……" style="width: 20%" clearable/>
-            <tr></tr>
-            站点2:
-            <el-input v-model="search2" placeholder="请输入站点2的名称……" style="width: 20%" clearable/>
-            <el-button type="primary" style="margin: 5px" @click="load">搜索直达线路名称</el-button>
-        </div>
-        <tr></tr>
-        <el-table :data="tableData" border stripe style="width: 25%">
-            <el-table-column prop="lines" label="路线名称" sortable/>
-        </el-table>
+  <!--    给两个站点，返回直达的公交车路线名-->
+  <div style="padding: 10px">
+    <!--        搜索框-->
+    <div style="margin: 10px 0">
+      站点1:
+      <el-input v-model="search1" placeholder="请输入站点1的名称……" style="width: 20%" clearable/>
+      <tr></tr>
+      站点2:
+      <el-input v-model="search2" placeholder="请输入站点2的名称……" style="width: 20%" clearable/>
+      <el-button type="primary" style="margin: 5px" @click="load">搜索直达线路名称</el-button>
     </div>
+    <tr></tr>
+    <el-table :data="tableData" border stripe style="width: 25%">
+      <el-table-column prop="name" label="路线名称" sortable/>
+      <el-table-column prop="directional" label="路线方向" />
+    </el-table>
+  </div>
 </template>
 
 <script>
-    import request from "@/utils/request";
+import request from "@/utils/request";
 
-    export default {
-        name: "DirectRoute",
-        components: {},
-        data() {
-            return {
-                search1: '',
-                search2: '',
-                tableData: [],
-            }
-        },
-        created() {  //页面加载的时候调用load
-            this.load();
-        },
-        methods: {
-            load() {     //给到一个url和参数
-                request.get("getTwoStationDirectRoutenameByName", {
-                    params: {
-                        name1: this.search1,
-                        name2: this.search2,
-                    }
-                }).then(res => {  //结果放在.then里面，通过=>取后台返回结果
-                    this.tableData = []
-                    console.log(res)
-                    // this.tableData = res.data
-                    if (res.data.length == 0)
-                        this.$message.warning("不存在直达线路！请输入新的数据再次尝试！")
-                    res.data.forEach(item => {
-                        this.tableData.push({lines: item})
-                    })
-                })
-            },
-        }
+export default {
+  name: "DirectRoute",
+  components: {},
+  data() {
+    return {
+      search1: '',
+      search2: '',
+      tableData: [],
     }
+  },
+  created() {  //页面加载的时候调用load
+    this.load();
+  },
+  methods: {
+    load() {     //给到一个url和参数
+      request.get("/StationLine/directpathbetweentwostations/" + this.search1 + "-" + this.search2, {
+        params: {
+          name1: this.search1,
+          name2: this.search2,
+        }
+      }).then(res => {  //结果放在.then里面，通过=>取后台返回结果
+        console.log(this.search1 + " " + this.search2)
+        console.log(res.data)
+        if (res.data.code == "00000") {
+          this.$message.success("查询成功！")
+          this.tableData = res.data.data
+        } else if (this.search1!="" && this.search2!=" " &&res.data.code == "A0001")
+          this.$message.warning("不存在直达线路！请输入新的数据再次尝试！")
+      })
+    },
+  }
+}
 </script>
 
 <style scoped>
